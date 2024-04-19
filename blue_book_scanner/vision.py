581e943f9be7d345df62ba78687bc496fb6926f4
dansterdam <dansterdam.art@gmail.com>
2024-04-19 16:14:37 +0200

    add missing pages from 1940s, use gpt-4-turbo

--- blue_book_scanner/vision.py
+++ blue_book_scanner/vision.py
@@ -147,7 +147,7 @@ def encode_pdf_page_to_base64_image(page):
 
 def gpt_ocr(image, filename, client):
     response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
+        model="gpt-4-turbo",
         messages=[
             {
                 "role": "user",
