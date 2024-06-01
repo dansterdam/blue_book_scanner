a2d8d5c1b9041cc0938e7394e504628b11f9adcf
dansterdam <dansterdam.art@gmail.com>
2024-06-01 14:19:43 +0200

    use anthropic api and claude haiku in vision.py

--- blue_book_scanner/vision.py
+++ blue_book_scanner/vision.py
@@ -1,9 +1,11 @@
+import anthropic
 import base64
 import os
 import json
 import io
 import re
 import PyPDF2
+from PIL import Image
 import argparse
 import concurrent.futures
 from time import sleep
@@ -36,8 +38,13 @@ def main():
     os.makedirs(args.output, exist_ok=True)
 
     with open(args.keyfile) as f:
-        key = json.load(f)["api_key"]
-    client = OpenAI(api_key=key)
+        keys = json.load(f)
+        openai_key = keys["openai_api_key"]
+        anthropic_key = keys["anthropic_api_key"]
+    #client = OpenAI(api_key=openai_key)
+    client = anthropic.Anthropic(
+    api_key=anthropic_key,
+)
     # OpenAI API Key
 
     process_fileset(args.input, args.output, client)
@@ -46,7 +53,7 @@ def main():
 def process_fileset(input_dir, output_dir, client):
     files = [i for i in os.listdir(input_dir)]
     # Define the maximum number of threads based on your system's capability and the nature of the task
-    max_workers = 3  # Adjust this based on your needs and system's capabilities
+    max_workers = 4  # Adjust this based on your needs and system's capabilities
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         # Submit each file to be processed in parallel
@@ -62,7 +69,7 @@ def process_single_file(file, input_dir, output_dir, client):
 
     # Retrieve pages already processed to avoid reprocessing
     pages_done = [int(re.search(r'pdf(\d+).txt', filename).group(1)) for filename in [i for i in os.listdir(output_dir) if file in i] if re.search(r'pdf(\d+).txt', filename)]
-    max_workers = 1
+    max_workers = 2
     pages_not_done = [i for i in range(1, pages_in_pdf + 1) if i not in pages_done]
     # Using ThreadPoolExecutor to parallelize page processing
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -102,7 +109,9 @@ def convert_page_to_image(pdf_path, page_number):
 
 def encode_image_to_base64(image):
     img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format="PNG")
+    image = image.resize((1024,768), Image.LANCZOS)
+    image.save(img_byte_arr, format="JPEG", quality=85)
+    
     return base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
 
 
@@ -112,17 +121,19 @@ def process_image_and_extract_text(
     retries = 0
     while retries < max_retries:
         try:
-            response = gpt_ocr(base64_image, output_filepath, client)
-            gpt_output_text = response.choices[0].message.content
+            #response = gpt_ocr(base64_image, output_filepath, client)
+            #gpt_output_text = response.choices[0].message.content
+            response = anthropic_ocr(base64_image, output_filepath, client)
+            anthropic_output_text = response.content[0].text
             with open(output_filepath, "w") as f:
-                f.write(gpt_output_text)
+                f.write(anthropic_output_text)
             break  # Success, break out of the retry loop
         except KeyError as e:
             print(f"Error processing {output_filepath}: {e}")
             retries += 1
             sleep(retry_delay)  # Wait before retrying
         except Exception as e:
-            print(f"Unexpected error: {e}")
+            print(f"Unexpected error processing {output_filepath}: {e}")
             break  # Break on unexpected errors
 
     if retries == max_retries:
@@ -138,16 +149,42 @@ def get_pdf_page_count(pdf_path):
 def encode_pdf_page_to_base64_image(page):
     # Convert image to bytes
     img_byte_arr = io.BytesIO()
-    page.save(img_byte_arr, format="PNG")
+    page.save(img_byte_arr, format="JPEG", quality=85)
     img_byte_arr = img_byte_arr.getvalue()
 
     # Encode to base64 and add to the list
     return base64.b64encode(img_byte_arr).decode("utf-8")
 
+def anthropic_ocr(image, filename, client):
+
+    response = client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=2000,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                        {
+                        "type": "text",
+                        "text": f"This is a page in an old UFO report document from project blue book. Please describe any photograph that is present. Not every image will contain a photograph. Then, please act as an OCR system and produce and output all the text found in the document and nothing else. For context: the filename including page number is {filename}"
+                    },
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/jpeg",
+                            "data": image,
+                        },
+                    }
+                ],
+            }
+        ],
+    )
+    return response
 
 def gpt_ocr(image, filename, client):
     response = client.chat.completions.create(
-        model="gpt-4-turbo",
+        model="gpt-4o",
         messages=[
             {
                 "role": "user",
